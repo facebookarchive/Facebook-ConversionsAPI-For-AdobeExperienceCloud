32b784e3231e0602e3cd98b0d93157f0000371ab
Cecilia Wang <chongchw@fb.com>
2021-04-14 16:55:03 -0700

    import js-sha256 in different way
    Summary: Adobe launch platform will not install all node modules, import js-sha so the adobe packer will include the js-sha256 folder
    
    Reviewed By: verswu
    
    Differential Revision: D27781623
    
    fbshipit-source-id: d414d5b1a9540a6196c8f3d32b6853f61e4a757f

--- src/lib/actions/shaHashingHelper.js
+++ src/lib/actions/shaHashingHelper.js
@@ -2,7 +2,7 @@
  * (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
  */
 
-var sha256 = require('js-sha256');
+var sha256 = require('./../../../node_modules/js-sha256/build/sha256.min.js');
 
 module.exports = (data) => {
     if (data.length === 64 && /^[0-9A-Za-z]*$/.test(data)) {
