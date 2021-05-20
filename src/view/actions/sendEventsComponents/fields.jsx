64251776cd8bd85de728e0795f9661ce8d1a54e8
Kent Wu (SE) <wuk@fb.com>
2021-05-20 10:58:43 -0700

    Adobe CAPI extension quick fixes
    Summary:
    - fixed focus issue by removing Dialog component
    - updated display name
    - removed trailing space
    
    Reviewed By: CcWang
    
    Differential Revision: D28548671
    
    fbshipit-source-id: e35e83a8c2801c3a9300d4bb520adb6abf0cbfc1

--- src/view/actions/sendEventsComponents/fields.jsx
+++ src/view/actions/sendEventsComponents/fields.jsx
@@ -8,7 +8,6 @@ import {useFormContext} from 'react-hook-form';
 import {
   Checkbox,
   Content,
-  Dialog,
   Divider,
   Flex,
   Heading,
@@ -52,128 +51,123 @@ export default () => {
     test ID here to start seeing event activity appear in the Test Events window.`;
 
   return (
-    <Dialog>
-      <Flex direction="row" gap="size-200">
-        <Flex direction="column" gap="size-65">
-          <Heading> Facebook Event Setup </Heading>
-          <Divider size="S" />
-          <Content>
-            <Text>{facebookEventSetupText}</Text>
+    <Flex direction="row" gap="size-200">
+      <Flex direction="column" gap="size-65">
+        <Heading> Facebook Event Setup </Heading>
+        <Divider size="S" />
+        <Content>
+          <Text>{facebookEventSetupText}</Text>
+          <Link>
+            <a href={parametersURI} target="_blank" rel="noreferrer">
+              Facebook for Developers.
+            </a>
+          </Link>
+        </Content>
+
+        <Heading marginTop="1em"> Server Event Parameters</Heading>
+        <Divider size="S" />
+        <Content>
+          <Content marginBottom="1em">
+            <Text>{serverEventsParametersText}</Text>
             <Link>
-              <a href={parametersURI} target="_blank" rel="noreferrer">
-                Facebook for Developers.
+              <a href={serverEventsURI} target="_blank" rel="noreferrer">
+                documentation.
               </a>
             </Link>
           </Content>
-
-          <Heading marginTop="1em"> Server Event Parameters</Heading>
-          <Divider size="S" />
+          {serverEvents.map(([name, label], index) => {
+            return (
+              <WrappedTextField
+                key={index}
+                name={name}
+                component={TextField}
+                width="size-4600"
+                label={label}
+                supportDataElement
+              />
+            );
+          })}
+          <WrappedCheckboxComponent component={Checkbox} name="lduEnabled">
+            <Text>Enable Limited Data Use</Text>
+          </WrappedCheckboxComponent>
           <Content>
-            <Content marginBottom="1em">
-              <Text>{serverEventsParametersText}</Text>
+            <Text>
+              Learn more about{' '}
               <Link>
-                <a href={serverEventsURI} target="_blank" rel="noreferrer">
-                  documentation.
+                <a href={dpoURI} target="_blank" rel="noreferrer">
+                  Limited Data Use and Data Processing Options
                 </a>
               </Link>
-            </Content>
-            {serverEvents.map(([name, label], index) => {
-              return (
-                <WrappedTextField
-                  key={index}
-                  name={name}
-                  component={TextField}
-                  width="size-4600"
-                  label={label}
-                  supportDataElement
-                />
-              );
-            })}
-            <WrappedCheckboxComponent component={Checkbox} name="lduEnabled">
-              <Text>Enable Limited Data Use</Text>
-            </WrappedCheckboxComponent>
-            <Content>
-              <Text>
-                Learn more about{' '}
-                <Link>
-                  <a href={dpoURI} target="_blank" rel="noreferrer">
-                    Limited Data Use and Data Processing Options
-                  </a>
-                </Link>
-              </Text>
-            </Content>
+            </Text>
           </Content>
+        </Content>
 
-          <Heading marginTop="1em"> Customer Information Parameters </Heading>
-          <Divider size="S" />
-          <Content>
-            <Content marginBottom="1em">
-              <Text>{customerInformationText}</Text>
-              <Link>
-                <a
-                  href={customerInformationURI}
-                  target="_blank"
-                  rel="noreferrer">
-                  documentation.
-                </a>
-              </Link>
-            </Content>
-            {customerInformation.map(([name, label], index) => {
-              return (
-                <WrappedTextField
-                  key={index}
-                  name={name}
-                  component={TextField}
-                  width="size-4600"
-                  label={label}
-                  supportDataElement
-                />
-              );
-            })}
+        <Heading marginTop="1em"> Customer Information Parameters </Heading>
+        <Divider size="S" />
+        <Content>
+          <Content marginBottom="1em">
+            <Text>{customerInformationText}</Text>
+            <Link>
+              <a href={customerInformationURI} target="_blank" rel="noreferrer">
+                documentation.
+              </a>
+            </Link>
           </Content>
+          {customerInformation.map(([name, label], index) => {
+            return (
+              <WrappedTextField
+                key={index}
+                name={name}
+                component={TextField}
+                width="size-4600"
+                label={label}
+                supportDataElement
+              />
+            );
+          })}
+        </Content>
 
-          <Heading marginTop="1em"> Custom Data </Heading>
-          <Divider size="S" />
-          <Content>
-            <Content marginBottom="1em">
-              <Text>{customDataText}</Text>
-              <Link marginBottom="1em">
-                <a href={customDataURI} target="_blank" rel="noreferrer">
-                  documentation.
-                </a>
-              </Link>
-            </Content>
-            <WrappedTextField
-              name="customData"
-              component={TextArea}
-              width="size-4600"
-              label="Custom Data"
-              placeholder='{"currency": "usd", "value": 123.45}'
-              supportDataElement
-            />
+        <Heading marginTop="1em"> Custom Data </Heading>
+        <Divider size="S" />
+        <Content>
+          <Content marginBottom="1em">
+            <Text>{customDataText}</Text>
+            <Link marginBottom="1em">
+              <a href={customDataURI} target="_blank" rel="noreferrer">
+                documentation.
+              </a>
+            </Link>
           </Content>
+          <WrappedTextField
+            name="customData"
+            component={TextArea}
+            width="size-4600"
+            label="Custom Data"
+            placeholder='{"currency": "usd", "value": 123.45}'
+            supportDataElement
+          />
+        </Content>
 
-          <Heading marginTop="1em"> Test Event </Heading>
-          <Divider size="S" />
-          <Content>
-            <Content marginBottom="1em">
-              <Text>{testEventText}</Text>
-            </Content>
-            <WrappedCheckboxComponent component={Checkbox} name="isTestEvent">
-              Send as Test Event
-            </WrappedCheckboxComponent>
-            <WrappedTextField
-              name="testEventCode"
-              component={TextField}
-              width="size-4600"
-              label="Test Event Code"
-              isRequired
-              isDisabled={!isTestEvent}
-              necessityIndicator="label"
-            />
+        <Heading marginTop="1em"> Test Event </Heading>
+        <Divider size="S" />
+        <Content>
+          <Content marginBottom="1em">
+            <Text>{testEventText}</Text>
           </Content>
-        </Flex>
+          <WrappedCheckboxComponent component={Checkbox} name="isTestEvent">
+            Send as Test Event
+          </WrappedCheckboxComponent>
+          <WrappedTextField
+            name="testEventCode"
+            component={TextField}
+            width="size-4600"
+            label="Test Event Code"
+            isRequired
+            isDisabled={!isTestEvent}
+            necessityIndicator="label"
+          />
+        </Content>
       </Flex>
-    </Dialog>
+    </Flex>
   );
 };
